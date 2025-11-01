ace1d2c071218ada231da04747e3c82051cb76a0
lzp-b3416 <71969849+lzp-b3416@users.noreply.github.com>
2025-11-01 23:34:10 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
-window.SUPA_AUTO = {
-  url: 'https://mzsncogvajhccegtzagt.supabase.co',
-  anon: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16c25jb2d2YWpoY2NlZ3R6YWd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE4NDA0NTgsImV4cCI6MjA3NzQxNjQ1OH0.LsfPIxEZ72jduzY7BnV2rOVEEyxoQvWlt4ghu5MX06Q',       // 公开 key，前端可暴露
-  defaultRoom: 'lab-2025',
-  autoconnect: true
-};
+<script>
+  window.SUPA_AUTO = {
+    url: "https://mzsncogvajhccegtzagt.supabase.co",
+    anon: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16c25jb2d2YWpoY2NlZ3R6YWd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE4NDA0NTgsImV4cCI6MjA3NzQxNjQ1OH0.LsfPIxEZ72jduzY7BnV2rOVEEyxoQvWlt4ghu5MX06Q",
+    defaultRoom: "lab-2025",  // 改成你的默认房间码
+    autoconnect: true
+  };
+</script>
